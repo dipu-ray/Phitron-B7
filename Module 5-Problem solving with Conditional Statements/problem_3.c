e0e11e9776fa23697a11d91a7070c612c234cb26
dipu-ray <dipu34786@gmail.com>
2025-04-15 03:54:03 +0600

    To add problem 3 files

--- Module 5-Problem solving with Conditional Statements/problem_3.c
+++ Module 5-Problem solving with Conditional Statements/problem_3.c
@@ -2,7 +2,19 @@
 
 int main() {
     // Problem 3: P. First digit
-    
-    
+    int x;
+    scanf("%d", &x);
+
+    if(x > 999 && x <= 9999) {
+        int new_x = x / 1000;
+
+        if(new_x % 2 == 0) {
+            printf("EVEN\n");
+        }
+        else {
+            printf("ODD\n");
+        }
+    }
+
     return 0;
 }
