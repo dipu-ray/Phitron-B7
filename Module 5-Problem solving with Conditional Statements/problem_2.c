562dd5a957dd0c4c1ebc9478f7214fd614f6f43b
dipu-ray <dipu34786@gmail.com>
2025-04-15 03:16:49 +0600

    To add problem 2 file

--- Module 5-Problem solving with Conditional Statements/problem_2.c
+++ Module 5-Problem solving with Conditional Statements/problem_2.c
@@ -2,8 +2,15 @@
 
 int main() {
     //Problem 2: J. Multiples
+    int a, b;
+    scanf("%d %d", &a, &b);
 
-    
+    if(a % b == 0 || b % a == 0) {
+        printf("Multiples\n");
+    }
+    else {
+        printf("No Multiples\n");
+    }
 
     return 0;
 }
