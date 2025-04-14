4f6447f875e95676c3f686af6d7103c43cf93f72
dipu-ray <dipu34786@gmail.com>
2025-04-15 02:48:52 +0600

    Solved to problem 1

--- Module 5-Problem solving with Conditional Statements/problem_1.c
+++ Module 5-Problem solving with Conditional Statements/problem_1.c
@@ -2,5 +2,18 @@
 
 int main() {
     // I. Welcome for you with condition
+
+    int A, B;
+    scanf("%d %d", &A, &B);
+
+    if(A >= 0 && B <= 100) {
+        if(A >= B) {
+            printf("Yes\n");
+        }
+        else {
+            printf("No\n");
+        }
+    }
+
     return 0;
 }
