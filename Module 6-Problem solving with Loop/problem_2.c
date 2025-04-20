99e06a7bbda5ca930532b9c9a943d7c0939118ca
dipu-ray <dipu34786@gmail.com>
2025-04-20 16:36:17 +0600

    Make a program 2

--- Module 6-Problem solving with Loop/problem_2.c
+++ Module 6-Problem solving with Loop/problem_2.c
@@ -1,8 +1,31 @@
 #include <stdio.h>
 
 int main() {
-    // Problem 1: C. Even, Odd, Positive and Negative
-    
+    // Problem 2: C. Even, Odd, Positive and Negative
+    int n, x, even = 0, odd = 0, positive = 0, negative = 0;
+    scanf("%d", &n);
+
+    for(int i = 1; i <= n; i++) {
+        scanf("%d", &x);
+        if(x % 2 == 0) {
+            even++;
+        }
+        else {
+            odd++;
+        }
+
+        if(x > 0) {
+            positive++;
+        }
+        else if(x < 0){
+            negative++;
+        }
+    }
+
+    printf("Even: %d\n", even);
+    printf("Odd: %d\n", odd);
+    printf("Positive: %d\n", positive);
+    printf("Negative: %d\n", negative);
 
     return 0;
 }
