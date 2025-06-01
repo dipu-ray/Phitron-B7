29f1cd2de908debbff1945e743c13230295dfd92
dipu-ray <dipu34786@gmail.com>
2025-06-01 23:56:44 +0600

    To add notes for module 06

--- Introduction to C++ for DSA/Week 02-String, Class, Objects/Module 06-More about class and string/range_based.cpp
+++ Introduction to C++ for DSA/Week 02-String, Class, Objects/Module 06-More about class and string/range_based.cpp
@@ -2,7 +2,19 @@
 using namespace std;
 
 int main() {
+    // string s;
+    // cin >> s;
+    // for(int i = 0; i < s.size(); i++) {
+    //     cout << s << endl;
+    // }
+
+
+    // Shortcut way
     string s;
+    cin >> s;
+    for(char c:s) {
+        cout << c << endl;
+    }
 
     return 0;
 }
