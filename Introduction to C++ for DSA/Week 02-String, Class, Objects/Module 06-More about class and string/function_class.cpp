d01446221a2ec09790ed500ece604aa8be4c216b
dipu-ray <dipu34786@gmail.com>
2025-06-02 15:55:17 +0600

    To add notes for module 06

--- Introduction to C++ for DSA/Week 02-String, Class, Objects/Module 06-More about class and string/function_class.cpp
+++ Introduction to C++ for DSA/Week 02-String, Class, Objects/Module 06-More about class and string/function_class.cpp
@@ -1,25 +1,59 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// class Student {
+//     public:
+//     string name;
+//     int roll;
+
+//     Student(string name, int roll) {
+//         this->name = name;
+//         this->roll = roll;
+//     }
+
+//     void hello() {
+//         cout << "Hello from " << name << endl;
+//     }
+// };
+
+// int main() {
+//     Student sakib("Sakib Ahmed", 5);
+//     cout << sakib.name << endl;
+//     sakib.hello();
+//     Student rakib("Rakib Ahmed", 5);
+//     rakib.hello();
+
+//     return 0;
+// }
+
+
+
+// Total marks find from a student
 class Student {
     public:
     string name;
     int roll;
+    int math;
+    int english;
 
-    Student(string name, int roll) {
+    Student(string name, int roll, int math, int english) {
         this->name = name;
         this->roll = roll;
+        this->math = math;
+        this->english = english;
     }
 
-    void hello() {
-        cout << "Hello" << endl;
+    void total_marks() {
+        cout << "Total marks of " << name << " = " << math+english << endl;
     }
 };
 
 int main() {
-    Student sakib("Sakib Ahmed", 5);
+    Student sakib("Sakib Ahmed", 5, 80, 90);
     cout << sakib.name << endl;
-    sakib.hello();
+    sakib.total_marks();
+    Student rakib("Rakib Ahmed", 5, 90, 80);
+    rakib.total_marks();
 
     return 0;
 }
