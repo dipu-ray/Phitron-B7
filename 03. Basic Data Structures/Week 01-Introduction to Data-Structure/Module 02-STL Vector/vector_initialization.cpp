ab3b3e9277f1f1f2ef5933d06137e8368226c654
dipu-ray <dipu34786@gmail.com>
2025-06-21 19:21:04 +0600

    To add notes for module 02

--- 03. Basic Data Structures/Week 01-Introduction to Data-Structure/Module 02-STL Vector/vector_initialization.cpp
+++ 03. Basic Data Structures/Week 01-Introduction to Data-Structure/Module 02-STL Vector/vector_initialization.cpp
@@ -2,35 +2,35 @@
 using namespace std;
 
 int main() {
-    // // Type 1
+    // // Type 1 - O(1)
     // vector<int> v;
     // cout << v.size() << endl;
 
-    // // Type 2
+    // // Type 2 - O(N)
     // vector<int> v(5);
     // cout << v.size() << endl;
 
-    // // Type 3
+    // // Type 3 - O(N)
     // vector<int> v(5, -1);
     // for(int i = 0; i < v.size(); i++) {
     //     cout << v[i] << " ";
     // }
 
-    // // Type 4
+    // // Type 4 - O(N)
     // vector<int> v(5, -1);
     // vector<int> v2(v);
     // for(int i = 0; i < v2.size(); i++) {
     //     cout << v2[i] << " ";
     // }
 
-    // // Type 5
+    // // Type 5 - O(N)
     // int a[5] = {1, 2, 3, 4, 5};
     // vector<int> v(a, a+5);
     // for(int i = 0; i < v.size(); i++) {
     //     cout << v[i] << " ";
     // }
 
-    // Type 5: same work with vector
+    // Type 5: same work with vector  - O(N)
     vector<int> v = {1, 2, 3, 4, 5};
     for(int i = 0; i < v.size(); i++) {
         cout << v[i] << " ";
