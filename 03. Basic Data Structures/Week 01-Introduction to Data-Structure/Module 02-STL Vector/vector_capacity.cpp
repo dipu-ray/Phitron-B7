0f9f1eb236de621059d17f16ce423296a33bfff7
dipu-ray <dipu34786@gmail.com>
2025-06-21 15:50:08 +0600

    To add notes for module 02

--- 03. Basic Data Structures/Week 01-Introduction to Data-Structure/Module 02-STL Vector/vector_capacity.cpp
+++ 03. Basic Data Structures/Week 01-Introduction to Data-Structure/Module 02-STL Vector/vector_capacity.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 
 int main() {
+    // // capacity() function
     // vector<int> v;
     // cout << v.capacity() << endl;
 
@@ -23,14 +24,81 @@ int main() {
 
 
 
-    // Clear() function
+
+    // // clear() function
+    // vector<int> v;
+
+    // v.push_back(10);
+    // v.push_back(20);
+    // v.push_back(30);
+    // cout << v.size() << endl;
+
+    // v.clear();
+    // cout << v.size() << endl;
+
+    // // Check the element are not delete
+    // cout << v[0] << endl;
+    // cout << v[1] << endl;
+    // cout << v[2] << endl;
+
+
+
+
+    // // empty() function
+    // vector<int> v;
+    // if(v.empty()) {
+    //     cout << "True" << endl;
+    // }
+    // else {
+    //     cout << "False" << endl;
+    // }
+
+
+
+
+    // // resize() function
+    // vector<int> v;
+    // v.push_back(10);
+    // v.push_back(20);
+    // v.push_back(30);
+    // v.push_back(40);
+    // v.push_back(50);
+
+    // v.resize(3);
+
+    // for(int i = 0; i < v.size(); i++) {
+    //     cout << v[i] << endl;
+    // }
+
+
+
+
+    // // resize() function it can expand
+    // vector<int> v;
+    // v.push_back(10);
+    // v.push_back(20);
+    // v.push_back(30);
+
+    // v.resize(5);
+
+    // for(int i = 0; i < v.size(); i++) {
+    //     cout << v[i] << endl;
+    // }
+
+
+
+
+    // resize() function it can expand and you can value assign
     vector<int> v;
+    v.push_back(10);
+    v.push_back(20);
+    v.push_back(30);
 
-    v.push_back(10);
-    v.push_back(10);
-    v.push_back(10);
+    v.resize(5, 100);
 
-    cout << v.size() << endl;
+    for(int i = 0; i < v.size(); i++) {
+        cout << v[i] << endl;
+    }
 
     return 0;
 }
