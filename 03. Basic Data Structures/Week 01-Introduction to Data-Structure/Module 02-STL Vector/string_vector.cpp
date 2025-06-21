1d6ae60907514d56428ffab023d21b476c7a383d
dipu-ray <dipu34786@gmail.com>
2025-06-21 22:34:36 +0600

    To add notes for module 02

--- 03. Basic Data Structures/Week 01-Introduction to Data-Structure/Module 02-STL Vector/string_vector.cpp
+++ 03. Basic Data Structures/Week 01-Introduction to Data-Structure/Module 02-STL Vector/string_vector.cpp
@@ -2,7 +2,35 @@
 using namespace std;
 
 int main() {
+    // int n;
+    // cin >> n;
+
+    // vector<string> v(n);
+
+    // for(int i = 0; i < n; i++) {
+    //     cin >> v[i];
+    // }
+
+    // for(string s : v) {
+    //     cout << s << " ";
+    // }
+
+
+
+
     int n;
+    cin >> n;
+
+    cin.ignore();
+    vector<string> v(n);
+
+    for(int i = 0; i < n; i++) {
+        getline(cin, v[i]);
+    }
+
+    for(string s : v) {
+        cout << s << " ";
+    }
 
     return 0;
 }
