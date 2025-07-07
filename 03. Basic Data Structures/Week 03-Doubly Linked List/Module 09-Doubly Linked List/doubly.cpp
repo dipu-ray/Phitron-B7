f1c4f6eb7b51420c06fc4935797f10c92009b064
dipu-ray <dipu34786@gmail.com>
2025-07-07 22:53:28 +0600

    To add notes for module 09

--- 03. Basic Data Structures/Week 03-Doubly Linked List/Module 09-Doubly Linked List/doubly.cpp
+++ 03. Basic Data Structures/Week 03-Doubly Linked List/Module 09-Doubly Linked List/doubly.cpp
@@ -1,8 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+class Node {
+    public:
+    int value;
+    Node* next;
+    Node* previous; // Make a pointer to back to previous
+
+    Node(int value) {
+        this->value = value;
+        this->next = NULL;
+        this->previous = NULL; // Initial to NULL
+    }
+};
+
 int main() {
-    int x;
+    // Create doubly linked list
+    Node* head = new Node(10);
+    Node* a = new Node(20);
+    Node* tail = new Node(30);
+
+    // Connection doubly linked list
+    head->next = a;
+    a->previous = head;
+    a->next = tail;
+    tail->previous = a;
 
     return 0;
 }
