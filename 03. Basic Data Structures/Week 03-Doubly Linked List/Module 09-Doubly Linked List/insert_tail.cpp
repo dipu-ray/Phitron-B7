d21add7695698947324e85d68b0f07160fb8a500
dipu-ray <dipu34786@gmail.com>
2025-07-08 02:46:55 +0600

    To add notes for module 09

---
+++ 03. Basic Data Structures/Week 03-Doubly Linked List/Module 09-Doubly Linked List/insert_tail.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node {
+    public:
+    int value;
+    Node* next;
+    Node* previous; // Make a pointer to back to previous
+
+    Node(int value) {
+        this->value = value;
+        this->next = NULL;
+        this->previous = NULL; // Initial to NULL
+    }
+};
+
+void print_forward(Node* head) {
+    Node* tmp = head;
+    while(tmp != NULL) {
+        cout << tmp->value << " ";
+        tmp = tmp->next;
+    }
+    cout << endl;
+}
+
+void insert_tail(Node* &head, Node* &tail, int value) {
+    Node* newNode = new Node(value); // Make new node
+    tail->next = newNode;
+    newNode->previous = tail;
+    tail = newNode;
+}
+
+int main() {
+    Node* head = new Node(10);
+    Node* a = new Node(20);
+    Node* tail = new Node(30);
+
+    head->next = a;
+    a->previous = head;
+
+    a->next = tail;
+    tail->previous = a;
+
+    // Call to functions
+    insert_tail(head, tail, 100);
+    insert_tail(head, tail, 200);
+    print_forward(head);
+
+    return 0;
+}
