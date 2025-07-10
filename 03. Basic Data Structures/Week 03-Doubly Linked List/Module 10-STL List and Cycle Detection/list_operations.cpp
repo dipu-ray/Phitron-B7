5a18ce3d244bc2923b8f22ff4a6c64cfe341f586
dipu-ray <dipu34786@gmail.com>
2025-07-10 20:11:48 +0600

    To add notes for module 10

--- 03. Basic Data Structures/Week 03-Doubly Linked List/Module 10-STL List and Cycle Detection/list_operations.cpp
+++ 03. Basic Data Structures/Week 03-Doubly Linked List/Module 10-STL List and Cycle Detection/list_operations.cpp
@@ -9,9 +9,24 @@ int main() {
     //     cout << value << endl;
     // }
 
-    // Sort the list in ascending order
-    list<int> l = {10, 20, 30, 40, 50, 20};
+    // // Sort the list in ascending order
+    // list<int> l = {10, 20, 30, 40, 50, 20};
+    // l.sort();
+    // for(int value : l) {
+    //     cout << value << endl;
+    // }
+
+    // // Sort the list in descending order
+    // list<int> l = {10, 20, 30, 40, 50, 20};
+    // l.sort(greater<int>());
+    // for(int value : l) {
+    //     cout << value << endl;
+    // }
+
+    // Deletes the duplicate values from the list. You must sort the list first
+    list<int> l = {10, 20, 30, 40, 50, 20, 30, 10};
     l.sort();
+    l.unique();
     for(int value : l) {
         cout << value << endl;
     }
