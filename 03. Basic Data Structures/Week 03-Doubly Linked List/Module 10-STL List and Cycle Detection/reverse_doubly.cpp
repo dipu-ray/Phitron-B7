71f97818db6c9a7bfebaa3e4ff62fa36397459d1
dipu-ray <dipu34786@gmail.com>
2025-07-11 02:02:17 +0600

    To add notes for module 10

---
+++ 03. Basic Data Structures/Week 03-Doubly Linked List/Module 10-STL List and Cycle Detection/reverse_doubly.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node {
+    public:
+    int value;
+    Node* next;
+    Node* previous; // Make a pointer to back to previous
+
+    Node(int value) {
+        this->value = value;
+        this->next = NULL;
+        this->previous = NULL; // Initial to NULL
+    }
+};
+
+void print_forward(Node* head) {
+    Node* tmp = head;
+    while(tmp != NULL) {
+        cout << tmp->value << " ";
+        tmp = tmp->next;
+    }
+    cout << endl;
+}
+
+void insert_tail(Node* &head, Node* &tail, int value) {
+    Node* newNode = new Node(value); // Make new node
+
+    // When node was empty
+    if(head == NULL) {
+        head = newNode;
+        tail = newNode;
+        return;
+    }
+
+    tail->next = newNode;
+    newNode->previous = tail;
+    tail = newNode;
+}
+
+int main() {
+    Node* head = NULL;
+    Node* tail = NULL;
+
+    int value;
+    while(true) {
+        cin >> value;
+        if(value == -1) {
+            break;
+        }
+        insert_tail(head, tail, value);
+    }
+
+    print_forward(head);
+
+    return 0;
+}
