1a826ca3487cb82126606e40a0411eb92a1400cb
dipu-ray <dipu34786@gmail.com>
2025-07-10 16:48:44 +0600

    To add notes for module 10

--- 03. Basic Data Structures/Week 03-Doubly Linked List/Module 10-STL List and Cycle Detection/list_modifiers.cpp
+++ 03. Basic Data Structures/Week 03-Doubly Linked List/Module 10-STL List and Cycle Detection/list_modifiers.cpp
@@ -2,11 +2,28 @@
 using namespace std;
 
 int main() {
-    // Assign another list
-    list<int> l = {10, 20, 30};
-    list<int> l2;
-    l2.assign(l.begin(), l.end());
-    for(int value : l2) {
+    // // Assign another list
+    // list<int> l = {10, 20, 30};
+    // list<int> l2;
+    // l2 = l;
+    // // l2.assign(l.begin(), l.end());
+    // for(int value : l2) {
+    //     cout << value << endl;
+    // }
+
+    // // Add an element to the tail & add an element to the head.
+    // list<int> l = {10, 20, 30};
+    // l.push_back(40);
+    // l.push_front(5);
+    // for(int value : l) {
+    //     cout << value << endl;
+    // }
+
+    // Delete the tail & delete the head
+    list<int> l = {10, 20, 30, 40, 50};
+    l.pop_back();
+    l.pop_front();
+    for(int value : l) {
         cout << value << endl;
     }
 
