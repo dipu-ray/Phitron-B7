40808fd04dd01f87c27bf279fc9fd988a55dd0ba
dipu-ray <dipu34786@gmail.com>
2025-07-10 14:59:54 +0600

    To add notes for module 10

--- 03. Basic Data Structures/Week 03-Doubly Linked List/Module 10-STL List and Cycle Detection/stl_list.cpp
+++ 03. Basic Data Structures/Week 03-Doubly Linked List/Module 10-STL List and Cycle Detection/stl_list.cpp
@@ -20,8 +20,30 @@ int main() {
     //     cout << *it << endl;
     // }
 
-    // Using range based loops: Construct a list with N elements and the value will be V
-    list<int> l(10, 5);
+    // // Using range based loops: Construct a list with N elements and the value will be V
+    // list<int> l(10, 5);
+    // for(int value : l) {
+    //     cout << value << endl;
+    // }
+
+    // // Construct a list by copying another list list2
+    // list<int> l = {1, 2, 3, 4, 5};
+    // list<int> l2(l);
+    // for(int value : l2) {
+    //     cout << value << endl;
+    // }
+
+    // // Construct a list by copying all elements from an array A of size N
+    // list<int> l = {1, 2, 3, 4, 5};
+    // int a[] = {10, 20, 30};
+    // list<int> l2(a, a+3);
+    // for(int value : l2) {
+    //     cout << value << endl;
+    // }
+
+    // Construct a list by copying all elements from a vector v
+    vector<int> v = {10, 20, 30, 40, 50};
+    list<int> l(v.begin(), v.end());
     for(int value : l) {
         cout << value << endl;
     }
