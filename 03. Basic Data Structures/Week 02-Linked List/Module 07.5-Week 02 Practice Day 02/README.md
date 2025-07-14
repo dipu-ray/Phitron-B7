89125cc3af29d2eff33c4b64cefd977c83397165
dipu-ray <dipu34786@gmail.com>
2025-07-14 14:33:55 +0600

    To add notes for module 7.5

--- 03. Basic Data Structures/Week 02-Linked List/Module 07.5-Week 02 Practice Day 02/README.md
+++ 03. Basic Data Structures/Week 02-Linked List/Module 07.5-Week 02 Practice Day 02/README.md
@@ -7,7 +7,7 @@
 3. Insert at head Updated
 
 ## Practice Problem Set: Module 7.5
-- [Link](https://docs.google.com/document/d/1EXLsPmtlVXNjdqk49BCcLbPHf0n4UBlP/edit?usp=sharing&ouid=110071013354717279052&rtpof=true&sd=true)
+- [Link -](https://docs.google.com/document/d/1EXLsPmtlVXNjdqk49BCcLbPHf0n4UBlP/edit?usp=sharing&ouid=110071013354717279052&rtpof=true&sd=true)
 - <img src="./images/practice_problem.png" width="500">
 - <img src="./images/practice_problem2.png" width="500">
 - <img src="./images/practice_problem3.png" width="500">
@@ -19,7 +19,7 @@
 - Explanation those whole practice problems `1 to 5`
 
 ## 2. Solution of Practice Problem 4
-- Solved this problem 04
+- Solved this problem 04. Program: `practice_problem4.cpp`
 
 ## 3. Insert at head Updated
 - adffad
---
+++ 03. Basic Data Structures/Week 02-Linked List/Module 07.5-Week 02 Practice Day 02/practice_problem4.cpp
@@ -0,0 +1,100 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node {
+    public:
+    int value;
+    Node* next; 
+
+    Node(int value) {
+        this->value = value;
+        this->next = NULL;
+    }
+};
+
+// Function - Insert at head
+void insert_at_head(Node* &head, int value) {
+    Node* newNode = new Node(value); // Create a new node
+    newNode->next = head; // Going on next node
+    head = newNode; // Assign to head
+}
+
+// Function - Insert at tail
+void insert_at_tail(Node* &head, Node* &tail, int value) {
+    Node* newNode = new Node(value);
+    if(head == NULL) {
+        head = newNode;
+        return;
+    }
+    tail->next = newNode;
+    tail = tail->next;
+}
+
+// Function - Insert at any position
+void insert_at_any_position(Node* &head, int index, int value) {
+    Node* newNode = new Node(value);
+    Node* tmp = head;
+    for(int i = 0; i < index-1; i++) {
+        tmp = tmp->next;
+    }
+    newNode->next = tmp->next;
+    tmp->next = newNode;
+}
+
+// Function - Print linked list
+void print_linked_list(Node* head) {
+    Node* tmp = head;
+    while(tmp != NULL) {
+        cout << tmp->value << " ";
+        tmp = tmp->next;
+    }
+    cout << endl;
+}
+
+// Function - Find the size of linked list
+int size_linked_list(Node* head) {
+    int count = 0;
+    Node* tmp = head;
+    while(tmp != NULL) {
+        count++;
+        tmp = tmp->next;
+    }
+    return count;
+}
+
+int main() {
+    Node* head = NULL;
+    Node* tail = NULL;
+
+    int value;
+    while(true) {
+        cin >> value;
+        if(value == -1) {
+            break;
+        }
+        insert_at_tail(head, tail, value);
+    }
+
+    // Input with checking valid or invalid?
+    int idx, value;
+    while(cin >> idx >> value) {
+        int sz = size_linked_list(head);
+        if(idx > sz) {
+            cout << "Invalid\n";
+        }
+        else if(idx == sz) {
+            insert_at_tail(head, tail, value);
+            print_linked_list(head);
+        }
+        else if(idx == 0) {
+            insert_at_head(head, value);
+            print_linked_list(head);
+        }
+        else {
+            insert_at_any_position(head, idx, value);
+            print_linked_list(head);
+        }
+    }
+
+    return 0;
+}
