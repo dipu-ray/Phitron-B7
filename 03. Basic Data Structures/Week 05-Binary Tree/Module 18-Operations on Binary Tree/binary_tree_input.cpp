9b51af5a693d9d51195fef479e5e59c18f6f7946
dipu-ray <dipu34786@gmail.com>
2025-07-30 01:39:09 +0600

    To add notes for module 18

--- 03. Basic Data Structures/Week 05-Binary Tree/Module 18-Operations on Binary Tree/binary_tree_input.cpp
+++ 03. Basic Data Structures/Week 05-Binary Tree/Module 18-Operations on Binary Tree/binary_tree_input.cpp
@@ -13,6 +13,41 @@ public:
     }
 };
 
+void input_tree() {
+    int val;
+    cin >> val;
+    Node* root = new Node(val);
+    queue<Node*> q;
+    q.push(root);
+
+    while(!q.empty()) {
+        // 1. Ber kore ana
+        Node* p = q.front();
+        q.pop();
+
+        // 2. Oi node ke niye kaj
+        int l, r;
+        cin >> l >> r;
+        Node *myLeft, *myRight;
+
+        if(l == -1) myLeft = NULL;
+        else myLeft = new Node(l);
+        if(r == -1) myRight = NULL;
+        else myRight = new Node(r);
+
+        p->left = myLeft;
+        p->right = myRight;
+
+        // 3. Children push kora
+        if(p->left) {
+            q.push(p->left);
+        }
+        if(p->right) {
+            q.push(p->right);
+        }
+    }
+}
+
 int main() {
 
     return 0;
