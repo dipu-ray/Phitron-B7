bccb6c9f27017f356d14029a4ef79264d9573cda
dipu-ray <dipu34786@gmail.com>
2026-01-18 16:57:22 +0600

    To add STL priority queue codes

--- 03. Basic Data Structures/Week 06-BST, Heap and Map/Module 23-STL Priority Queue, Map and Set/priority_queue.cpp
+++ 03. Basic Data Structures/Week 06-BST, Heap and Map/Module 23-STL Priority Queue, Map and Set/priority_queue.cpp
@@ -2,7 +2,37 @@
 using namespace std;
 
 int main() {
-    int x
+    // // Maximum priority queue code...
+    // priority_queue<int> pq;
+    // pq.push(10);
+    // pq.push(5);
+    // pq.push(30);
+    // cout << pq.top() << endl;
+
+    // pq.push(100);
+    // cout << pq.top() << endl;
+
+    // // For remove first value in queue
+    // pq.pop();
+    // pq.pop();
+    // cout << pq.top() << endl;
+
+
+
+    // Minimum priority queue code...
+    priority_queue<int, vector<int>, greater<int>> pq;
+    pq.push(10);
+    pq.push(5);
+    pq.push(30);
+    cout << pq.top() << endl;
+
+    pq.push(2);
+    cout << pq.top() << endl;
+
+    // For remove first value in queue
+    pq.pop();
+    pq.pop();
+    cout << pq.top() << endl;
 
     return 0;
 }
